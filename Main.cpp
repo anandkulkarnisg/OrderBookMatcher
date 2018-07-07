debdd8e133b8d82b036025195149c4fbc5b397bd
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2018-07-07 09:29:47 +0800

    Anand Kulkarni - 07/07/2018 - Removed unwanted Files. Basic cleanup.

--- Main.cpp
+++ Main.cpp
@@ -38,11 +38,7 @@ int main(int argc, char* argv[])
 	results.emplace_back(pool.enqueue(func));
 	std::cout << "Main Thread id = " << std::this_thread::get_id() << std::endl;
 	for(auto&& iter : results)
-	{
 		iter.get();
-		//iter.first.get();	
-		//std::cout << "The execution from thread pool took " << iter.second << " milli seconds" << std::endl;
-	}
 	return(0);
 }
 
